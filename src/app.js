7aed59568802475700d7330f31c00ad7c9949345
Irshad Ali <irshad@phonerefer.com>
2020-03-16 17:59:00 +0530

    First commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+ // Service  Worker
+navigator.serviceWorker && navigator.serviceWorker.register('SW.js').then(function (registration) {
+
+});
+
+ // Char count
+ function countChars(countfrom, displayto) {
+    var len = document.getElementById(countfrom).value.length;
+    document.getElementById(displayto).innerHTML = len;
+}
+
+// Reset Button Reloads The Page
+function refreshPage() {
+    window.location.reload();
+}
+
+// Copy To Clipboard
+function copyClipboard() {
+    let copyText = document.getElementById("data"); // Get the text field
+    copyText.select(); // Select the text field
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    document.execCommand("copy"); // Copy the text inside the text field
+}
+
+// Mail Button
+function sendEmail() {
+    window.location = "mailto:irshad@phonerefer.com";
+}
