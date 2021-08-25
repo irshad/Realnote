490dd02e8db73ad4e1c5970cee8d10f0af05fde9
Irshad Ali <irshadali786143@gmail.com>
2021-08-25 18:08:15 +0530

    Update Flag.svelte

--- src/components/Flag.svelte
+++ src/components/Flag.svelte
@@ -16,6 +16,8 @@
         top: 20px;
         right: 20px;
         position: absolute;
+        overflow: hidden;
+        border-radius: 5px;
         cursor: pointer;
         transition: all 0.6s ease-in-out;
         box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
